cc354766bc0d2d6a2da7d01731745192bd5abb99
Taite Clark <taiteclark1@gmail.com>
2018-07-16 21:28:32 -0400

    Add display of artist, and check metadata upon starting the widget

--- playerctl.lua
+++ playerctl.lua
@@ -4,10 +4,17 @@ local lgi = require("lgi")
 local Playerctl = lgi.Playerctl
 local player = Playerctl.Player{}
 local playerctl_widget = wibox.widget.textbox()
-    
-player.on_metadata = function()
-    playerctl_widget:set_text(player:get_title())
+
+update_metadata = function()
+    if player:get_title() then
+	playerctl_widget:set_text(player:get_artist() .. " – " .. player:get_title())
+    else
+	playerctl_widget:set_text('-')
+    end
 end
+
+player.on_metadata = update_metadata
+update_metadata()
 
 return playerctl_widget
 
